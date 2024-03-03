e32c3e1aa55484eee4a8a5a50c4cf00a7272c814
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-03-03 18:14:18 +0900

    コメント追加、nav作成

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,51 @@
 import './Nav.scss';
-import Button from 'react-bootstrap/Button'; // react用bootstrap（ボタンコンポーネント）をインポート
+import { Button, ButtonToolbar } from 'react-bootstrap'; // react用bootstrap（ボタンコンポーネント）をインポート
 import { useState } from 'react';
 export default ({ props }) => {
+  let [flg, setFlg] = useState(false);
+  const showSubNav = () => {
+    setFlg((flg = !flg));
+  };
   return (
+    // フラグメント
     <>
       <nav className='navbar navbar-expand-md navbar-dark bg-dark sticky-top'>
         <div className='container'>
-          <a>
-            <Button>{props.title1}</Button>
+          <a className='navbar-brand' href='./'>
+            {props.title1}
           </a>
+          <Button
+            onClick={() => {
+              showSubNav();
+            }}
+            className='navbar-toggler'
+            data-toggle='collapse'
+            data-target='#navbar-content'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </Button>
+          <div
+            className='collapse navbar-collapse'
+            id='navbar-content'
+            // インラインスタイル適応
+            style={{ display: flg ? 'block' : 'none' }}
+          >
+            <ul className='navbar-nav me-auto'>
+              <li className='nav-item'>
+                <a className='nav-link nav__link' href='./'>
+                  TOP
+                </a>
+              </li>
+            </ul>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <form>
+                  <input type='text' />
+                  <button>検索</button>
+                </form>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
